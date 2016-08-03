2879182b496dc53cf9780bee64d8d02a6115326c
dfdf <dfdf>
2016-08-03 16:12:35 +0800

    1、方格游戏初始文件

---
+++ games/zzfk/js/test.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Administrator on 2016/8/3.
+ * author yaoqianfeng
+ */
+//测试文件
+function test(){
+
+}
+
+(function(){
+    var touche=new touch();
+    touche.eventHandle("add",$$("start-btn"),"touchstart",function(){
+        console.info("启动按钮");
+    },false);
+    touche.eventHandle("add",$$("pause-btn"),"touchstart",function(){
+        console.info("暂停按钮");
+    },false);
+    touche.eventHandle("add",$$("stop-btn"),"touchstart",function(){
+        console.info("停止按钮");
+    },false);
+    touche.eventHandle("add",$$("restart-btn"),"touchstart",function(){
+        console.info("重启按钮");
+    },false);
+
+})();
