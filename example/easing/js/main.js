3e3060797e99958cc831ff9e467dc32f17e7b868
dfdf <dfdf>
2016-09-01 18:47:54 +0800

    1、添加文件。

---
+++ example/easing/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/9/1.
+ * author yaoqianfeng
+ */
+$(function(){
+    $(".event .btn").live({
+        "click":function(event){
+            var _self=this;
+
+        }
+    });
+});
