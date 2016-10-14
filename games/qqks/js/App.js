588b2a32f65453d41378a40c9146492575db9b60
dfdf <dfdf>
2016-10-14 17:53:32 +0800

    1、添加游戏结构文件

--- games/qqks/js/App.js
+++ games/qqks/js/App.js
@@ -1,13 +1,35 @@
 /*对象、状态/行为、属性*/
-var app;
+
 //开始、暂停、重新启动、停止功能
 function App(){
+    var _self = this;
+    _self.isPaper=(function(){
+        appConfig.canvas=$$(appConfig.container);
+        appConfig.canvas.width = window.innerWidth;
+        appConfig.canvas.height = window.innerHeight;
 
+        if (!appConfig.canvas.getContext) {
+            return false;
+        }
+        appConfig.context = appConfig.canvas.getContext('2d');
+        return true;
+    })();
 }
 
 /*开始*/
 App.prototype.start=function(){
-
+    var _self = this;
+    if(!appConfig.isShowDebugInfo){
+        $$("debug-tool").style.display="none";
+    }
+    if(_self.isPaper){
+        _self.game= new game();
+        var touche = new touch();
+        touche.eventHandle("add", document, "touchstart", function (e) {
+            console.info("touchstart event");
+        }, false);
+        _self.game.start();
+    }
 };
 
 /*暂停*/
@@ -24,3 +46,10 @@ App.prototype.restart=function(){
 App.prototype.stop=function(){
 
 };
+
+
+var application=(function () {
+    var appInstance = new App();
+    return appInstance;
+})();
+application.start();
--- games/qqks/js/Button.js
+++ games/qqks/js/Button.js
@@ -2,3 +2,8 @@
 function Button(){
     
 }
+
+/*绑定点击某个区域,以使某个方法生效*/
+Button.prototype.touch=function(){
+
+}
