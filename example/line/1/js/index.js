06502734559a0b2cc8e4828ac4633d5ef176ee70
yqf <116091iyh@gmail.com>
2017-05-19 14:56:45 +0800

    修改

--- example/line/1/js/index.js
+++
@@ -1,7 +0,0 @@
-d3.selectAll("p").style("color", "red");
-d3.select("body").style("background-color", "black");
-d3.select("body")
-    .selectAll("p")
-    .data([4, 8, 15, 16, 23, 42])
-    .enter().append("p")
-    .text(function(d) { return "I’m number " + d + "!"; });
