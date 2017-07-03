84e1ac8902fe6fba81c89a96974f607e39b8b593
yqf <116091iyh@gmail.com>
2017-07-03 16:06:12 +0800

    预览上传之前的图片。

--- example/uploadPreviewImg/js/index.js
+++ example/uploadPreviewImg/js/index.js
@@ -2,7 +2,7 @@
 function setImagePreview(avalue) {
     var docObj = document.getElementById("doc");
     var imgObjPreview = document.getElementById("preview");
-    var divs = document.getElementById("localImag");
+    var localImgId = document.getElementById("localImg");
     //动态创建多个img
     var tempImg;
     if (docObj.files && docObj.files[0]) {
@@ -11,22 +11,19 @@ function setImagePreview(avalue) {
             tempImg.className="preview";
             tempImg.style.height = '100px';
             tempImg.src = window.URL.createObjectURL(docObj.files[i]);
-            divs.appendChild(tempImg);
+            localImgId.appendChild(tempImg);
         }
     } else {
         //IE下，使用滤镜
         docObj.select();
         var imgSrc = document.selection.createRange().text;
-        var localImagId = document.getElementById("localImag");
         //必须设置初始大小
-        localImagId.style.width = "100px";
-        localImagId.style.height = "100px";
+        localImgId.style.height = "100px";
         //图片异常的捕捉，防止用户修改后缀来伪造图片
         try {
-            localImagId.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)";
-            localImagId.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = imgSrc;
+            localImgId.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)";
+            localImgId.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = imgSrc;
         } catch (e) {
-            alert("您上传的图片格式不正确，请重新选择!");
             return false;
         }
         imgObjPreview.style.display = 'none';
