dd00f482fd9aa607ce33f902e18526fc51f4b41a
yqf <116091iyh@gmail.com>
2017-07-03 14:44:20 +0800

    预览上传之前的图片。

---
+++ example/uploadPreviewImg/js/index.js
@@ -0,0 +1,37 @@
+//下面用于图片上传预览功能
+function setImagePreview(avalue) {
+    //input
+    var docObj = document.getElementById("doc");
+//img
+    var imgObjPreview = document.getElementById("preview");
+    //div
+    var divs = document.getElementById("localImag");
+    if (docObj.files && docObj.files[0]) {
+        //火狐下，直接设img属性
+        imgObjPreview.style.display = 'block';
+        imgObjPreview.style.width = '100px';
+        imgObjPreview.style.height = '100px';
+        //imgObjPreview.src = docObj.files[0].getAsDataURL();
+        //火狐7以上版本不能用上面的getAsDataURL()方式获取，需要一下方式
+        imgObjPreview.src = window.URL.createObjectURL(docObj.files[0]);
+    } else {
+        //IE下，使用滤镜
+        docObj.select();
+        var imgSrc = document.selection.createRange().text;
+        var localImagId = document.getElementById("localImag");
+        //必须设置初始大小
+        localImagId.style.width = "100px";
+        localImagId.style.height = "100px";
+        //图片异常的捕捉，防止用户修改后缀来伪造图片
+        try {
+            localImagId.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)";
+            localImagId.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = imgSrc;
+        } catch (e) {
+            alert("您上传的图片格式不正确，请重新选择!");
+            return false;
+        }
+        imgObjPreview.style.display = 'none';
+        document.selection.empty();
+    }
+    return true;
+}
