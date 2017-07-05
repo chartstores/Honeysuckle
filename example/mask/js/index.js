bab5f69b98f268ea4bff6deba75c211eea79d5e2
yqf <116091iyh@gmail.com>
2017-07-05 11:41:09 +0800

    添加遮罩层，点击阴影部分删除整个遮罩。

---
+++ example/mask/js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".mask").on("click",function(event){
+
+    });
+
+    $(".mask-bg").on("click",function(event){
+        $(this).hide();
+        $(".mask").hide();
+    });
+});
